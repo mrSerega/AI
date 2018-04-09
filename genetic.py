89c5c264573c0921bd6795818de560d1a5e1f5c9
mrSerega <sega00796@mail.ru>
2018-04-09 11:06:40 +0300

    new iteration

--- genetic.py
+++ genetic.py
@@ -9,6 +9,8 @@ plt.ion()
 limit = 100
 number = 15
 exit_num = 100000
+population_number = 2000
+mutate_precent = 0.10 
 
 def get_random_point():
     res = []
@@ -16,7 +18,7 @@ def get_random_point():
     res.append(int(random.random()*limit))
     return res
 
-def get_random_filed():
+def get_random_field():
     res = []
     for i in range(number):
         res.append(get_random_point())
@@ -25,45 +27,69 @@ def get_random_filed():
 def get_distance(point1, point2):
     return math.sqrt((point1[0]-point2[0])**2 + (point1[1]-point2[1])**2)
 
-def get_cost(filed):
+def get_cost(field):
     sum = 0
-    for index in range(len(filed)-1):
-        sum += get_distance(filed[index],filed[index+1])
+    for index in range(len(field)-1):
+        sum += get_distance(field[index],field[index+1])
     return sum
 
-def do_random(filed, mode = 0):
+def mutate(field, mode = 0):
     if mode == 0:
-        first = int(random.random()*len(filed)) - 1
-        second = int(random.random()*len(filed)) - 1
+        first = int(random.random()*len(field)) - 1
+        second = int(random.random()*len(field)) - 1
 
-        filed[first], filed[second] = filed[second], filed[first]
+        field[first], field[second] = field[second], field[first]
     elif mode == 1:
-        random.shuffle(filed)
+        random.shuffle(field)
 
-filed = get_random_filed()
+def cross(parent1, parent2):
+    child = parent1[0:len(parent1)/2]
+    for gen in parent2:
+        if gen not in child: child.append(gen)
+    return child
 
-last_cost = sys.float_info.max
-last_state = filed[:]
-last_state_index = 0
-index = 0
+def select(population):
+    population.sort(key=get_cost)
+    return population[0:population_number]
 
-while ((index - last_state_index) < exit_num):
-    index +=1
-    last_state = filed[:]
-    do_random(filed)
-    current_cost = get_cost(filed)
-    if current_cost >= last_cost: filed = last_state[:]
-    else:
-        last_state_index = index
-        last_cost= current_cost
+adam = get_random_field() #first guy
+
+population = []
+
+for subject in range(population_number): #create first population
+    population.append(adam[:])
+    mutate(population[-1],1)
+
+generation = 0
+last_cost = 0
+
+while (generation < 50):
+    generation +=1
+    #mutate
+    random.shuffle(population)
+    for index in range(int(population_number*mutate_precent)):
+        population.append(population[index][:])
+        mutate(population[-1])
+    # find boys and girls
+    random.shuffle(population) 
+    #make love
+    for index in range(len(population)):
+        population.append(cross(population[index],
+                                population[population_number-index-1])
+                        )
+    population = select(population)
+
+    if last_cost != get_cost(population[0]):
         plt.clf()
-        plt.plot([el[0] for el in filed],[el[1] for el in filed]) 
+        plt.plot([el[0] for el in population[0]],[el[1] for el in population[0]]) 
         plt.show()
         plt.pause(0.1)
-    print ('best solution: {}, new solutin: {}'.format(last_cost,current_cost))
+        last_cost = population[0]
+    # print (len(population))
+    print('gen {}: {}'.format(generation,get_cost(population[0])))
 
 print ('done!')
 plt.ioff()
 plt.clf()
-plt.plot([el[0] for el in filed],[el[1] for el in filed]) 
+plt.plot([el[0] for el in population],[el[1] for el in population]) 
 plt.show()
